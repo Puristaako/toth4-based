6fdf2270e1c6845390ddca889e3bfe50390507eb
Aleksey <caminodeflores@gmail.com>
2018-03-24 01:20:26 +0500

    data_container -> st-stats
    st-stats non visible at start and can be show/hide

--- graphics/elements/st-stats/st-stats.js
+++ graphics/elements/st-stats/st-stats.js
@@ -3,13 +3,13 @@
 	'use strict';
 
 	nodecg.listenFor('showStats', function () {
-		document.getElementById("data_container").innerHTML = "SHOWED";
-		document.getElementById("data_container").style.display = "block";
+		document.getElementById("st-stats").innerHTML = "SHOWED";
+		document.getElementById("st-stats").style.display = "block";
 	});
 
 	nodecg.listenFor('hideStats', function () {
-		document.getElementById("data_container").innerHTML = "HIDDED";
-		document.getElementById("data_container").style.display = "none";
+		document.getElementById("st-stats").innerHTML = "HIDDED";
+		document.getElementById("st-stats").style.display = "none";
 	});
 
 })();
