b6d054fa0214d47addb33e5e00dba32956586743
Aleksey <caminodeflores@gmail.com>
2018-03-24 13:54:39 +0500

    worked show/hide
    without internal logic

--- graphics/elements/st-stats/st-stats.js
+++ graphics/elements/st-stats/st-stats.js
@@ -1,15 +1,37 @@
-
 (function () {
 	'use strict';
 
-	nodecg.listenFor('showStats', function () {
-		document.getElementById("st-stats").innerHTML = "SHOWED";
-		document.getElementById("st-stats").style.display = "block";
-	});
+	Polymer({
+		is: 'st-stats',
+<<<<<<< HEAD
 
-	nodecg.listenFor('hideStats', function () {
-		document.getElementById("st-stats").innerHTML = "HIDDED";
-		document.getElementById("st-stats").style.display = "none";
-	});
+		properties: {},
 
+		ready() {
+			const self = this;
+
+			nodecg.listenFor('showStats', function() {
+				document.getElementById('st-stats').style.display = 'block';
+				self.$.data_container.innerHTML = 'SHOWED';
+			});
+
+=======
+
+		properties: {},
+
+		ready() {
+			const self = this;
+
+			nodecg.listenFor('showStats', function() {
+				document.getElementById('st-stats').style.display = 'block';
+				self.$.data_container.innerHTML = 'SHOWED';
+			});
+
+>>>>>>> 0e1bbf992a20f3c702f84985a122b64e270a1bfd
+			nodecg.listenFor('hideStats', function() {
+				document.getElementById('st-stats').style.display = 'none';
+				self.$.data_container.innerHTML = 'HIDDEN';
+			});
+		}
+	})
 })();
